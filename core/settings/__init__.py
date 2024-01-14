e2195256659d8be0132736178514f9df62917a24
Egor Nikitin <eganik5674@gmail.com>
2024-01-15 00:43:10 +0300

    Update core/settings

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -6,23 +6,55 @@ from environs import Env
 
 @dataclass
 class Bots:
+    """
+    Дата класс для бота
+    """
+
     bot_token: str
 
 
 @dataclass
-class Settings:
+class Database:
+    """
+    Дата класс для базы данных
+    """
+
+    host_id: str
+    db_name: str
+    players_collection: str
+    words_collection: str
+
+
+@dataclass
+class BotSettings:
+    """
+    Дата класс для создания настроек для бота
+    """
+
     bots: Bots
 
 
-def get_settings(path: str):
+def get_bot_settings(path: str) -> BotSettings:
     env = Env()
     env.read_env(path)
 
-    return Settings(bots=Bots(bot_token=env.str("TELEBOT_API")))
+    return BotSettings(bots=Bots(bot_token=env.str("TELEBOT_API")))
+
+
+def get_db_settings(path: str) -> Database:
+    env = Env()
+    env.read_env(path)
+
+    return Database(
+        host_id=env.str("HOST_ID"),
+        db_name=env.str("DB_NAME"),
+        players_collection=env.str("PLAYERS_COLLECTION"),
+        words_collection=env.str("WORDS_COLLECTION"),
+    )
 
 
 TOKEN_PATH = Path(__file__).parent.parent.parent.resolve().joinpath("token")
+bot_settings = get_bot_settings(TOKEN_PATH)
 
-settings = get_settings(TOKEN_PATH)
-
-JSON_PATH = Path(__file__).parent.parent.resolve().joinpath("data", "results.json")
+DATABASE_PATH = Path(__file__).parent.parent.parent.resolve().joinpath("db_info")
+db_settings = get_db_settings(DATABASE_PATH)
