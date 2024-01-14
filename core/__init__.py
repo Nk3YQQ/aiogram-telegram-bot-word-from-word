ba7394c071f79ed96d44eb358f575192833690c7
Egor Nikitin <eganik5674@gmail.com>
2024-01-14 12:04:09 +0300

    Initial bot core

---
+++ core/__init__.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+
+from core.handler import GameHandler
+from core.settings import settings
+
+
+async def start():
+    bot = Bot(token=settings.bots.bot_token)
+    game = GameHandler()
+    dp = Dispatcher()
+
+    dp.message.register(game.start, Command(commands=["start"]))
+    dp.message.register(game.menu, F.text == "Меню")
+    dp.message.register(game.run, F.text == "Начать игру")
+    dp.message.register(game.help_info, F.text == "Помощь")
+    dp.message.register(game.max_result, F.text == "Максимальный результат")
+    dp.message.register(game.check_player_word, F)
+    try:
+        await dp.start_polling(bot)
+
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(start())
